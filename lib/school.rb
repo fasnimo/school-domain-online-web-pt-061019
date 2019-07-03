74a6caa089834f54781c3f30a3668f54d51436de
Rene Cocom <fasnimo@gmail.com>
2019-07-03 03:12:01 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -7,9 +7,10 @@ def initialize(name)
 @roster = {}
 end
 
-def add_student=(student)
-  @student = student
-end
+def add_student(student_name, grade)
+    roster[grade] ||= []
+    roster[grade] << student_name
+  end
 
 def
 
